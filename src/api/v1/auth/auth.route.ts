8766ca0bd2fa9762434414e5f3533ff805b7d789
Mikhail Goloto <qq@goloto.dev>
2024-03-27 20:02:55 +0300

    add auth/users route

--- src/api/v1/auth/auth.route.ts
+++ src/api/v1/auth/auth.route.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post('/register', authController.registration);
 router.post('/login', authController.login);
+router.get('/users', authController.getUsers);
 
 export { router as auth };
